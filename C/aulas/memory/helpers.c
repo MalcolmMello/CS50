71ebe46b136dfa1aff0c62de08ff8ebecdcebbe2
malcolm <malcolmmellolima@gmail.com>
2023-01-25 21:59:27 -0300

    sepia

--- C/aulas/memory/helpers.c
+++ C/aulas/memory/helpers.c
@@ -261,6 +261,64 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+// Sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+
+    RGBTRIPLE temp[height][width];
+
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            temp[i][j] = image[i][j];
+        }
+    }
+
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            int sepiaRed = 0;
+            int sepiaGreen = 0;
+            int sepiaBlue = 0;
+
+            sepiaRed = image[i][j].rgbtRed * 0.393 + image[i][j].rgbtGreen * 0.769 + image[i][j].rgbtBlue * 0.189;
+            sepiaGreen = image[i][j].rgbtRed * 0.349 + image[i][j].rgbtGreen * 0.686 + image[i][j].rgbtBlue * 0.168;
+            sepiaBlue = image[i][j].rgbtRed * 0.272 + image[i][j].rgbtGreen * 0.534 + image[i][j].rgbtBlue * 0.131;
+
+            if(sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if(sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if(sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+
+            temp[i][j].rgbtRed = sepiaRed;
+            temp[i][j].rgbtGreen = sepiaGreen;
+            temp[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
+
+
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = temp[i][j].rgbtRed;
+            image[i][j].rgbtGreen = temp[i][j].rgbtGreen;
+            image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
+        }
+    }
+    return;
+}
+
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -301,7 +359,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 for(int y = 0; y < 3; y++)
                 {
                     // Check for valid pixels
-                    if(i - 1 + x < 0 || i - 1 + x > height - 1 || j - 1 + y > width - 1)
+                    if(i - 1 + x < 0 || j - 1 + y < 0 || i - 1 + x > height - 1 || j - 1 + y > width - 1)
                     {
                         continue;
                     }
